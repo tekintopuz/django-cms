0cda8845b59ccd1147770580584f8d20d63fda5e
Tekin TOPUZ <tekintopuz@gmail.com>
2023-10-31 00:02:43 +0300

    add docker-compose

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'widget_tweaks',
     'dashboard',
     'dashboard.users',
@@ -71,6 +72,46 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'rosetta',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://w3cms.makaanlelo.com/",
+    "http://http://142.93.166.165/",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://w3cms.makaanlelo.com/",
+    "http://http://142.93.166.165/",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://w3cms.makaanlelo.com/",
+    "http://http://142.93.166.165/",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
 ]
 
 MIDDLEWARE = [
